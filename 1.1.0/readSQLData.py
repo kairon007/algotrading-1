56a4f1acd281b4301312a81c7cdad55c566960ce
Dipesh Bodas <dipesh.bodas@gmail.com>
2019-12-08 14:53:02 +0530

    completed SMA crossover algo logic pending work-append tables with daily ohlc

--- 1.1.0/readSQLData.py
+++ 1.1.0/readSQLData.py
@@ -7,6 +7,7 @@ from datetime import timedelta
 from pprintpp import pprint as pp
 import matplotlib.pyplot as plt 
 import itertools
+from tabulate import tabulate 
 
 mycursor = t.mydb.cursor()
 dates=[]
@@ -15,59 +16,65 @@ Totalweight=[]
 finalDate=[]
 finalWeight_10=[]
 finalWeight_05=[]
+price=[]
 avg = 0
 avg5=0
 Totalweight_5=[]
-sql_query = "select * from daily_ohlc where symbol = 'tcs' order by date1 asc"
+symbol = "CESC"
+sql_query = "select * from daily_ohlc where symbol = '"+str(symbol)+"'order by date1 asc"
 mycursor.execute(sql_query)
-records = mycursor.fetchall()
-count = 1;
+records_raw = mycursor.fetchall()
+records = records_raw
+count = 1
+count1 = 100
+count2=200
 isListFull = False
 w_avg5=[]
-print("Total number of rows in Laptop is: ", mycursor.rowcount)
+print("Crossover of ",count1," SMA and ",count2," SMA for ",symbol +"\n\n")
 for row in records:
 	#d1= datetime.datetime.strftime(row[1], '%d/%m')
 	d1=row[1]
 	d2= row[4]
-	if(isListFull==False and count<10):
+	price.append(d2)
+	if(isListFull==False and count<count2):
 		dates.append(d1)
 		Totalweight.append(d2)
 		finalDate.append(d1)
 		finalWeight_10.append(d2)
 		w_avg10.append(d2)
 		avg = avg+d2
-		if(count<6):
+		if(count<(count1+1)):
 			w_avg5.append(d2)
 			Totalweight_5.append(d2)
 			finalWeight_05.append(d2)
 			avg5 = avg5+d2
 		else:
 			avg5 = avg5+d2-Totalweight_5[0]
-			w_avg5.append(int(avg5/5))
+			w_avg5.append(int(avg5/count1))
 			w_avg5.pop(0)
 			Totalweight_5.pop(0)
-			finalWeight_05.append(int(avg5/5))
+			finalWeight_05.append(int(avg5/count1))
 			Totalweight_5.append(d2)
 		count=count+1
-	elif (isListFull == False and count==10):
+	elif (isListFull == False and count==count2):
 		w_avg5.pop(0)
 		avg5 = avg5+d2-Totalweight_5[0]
-		w_avg5.append(int(avg5/5))
-		finalWeight_05.append(int(avg5/5))
+		w_avg5.append(int(avg5/count1))
+		finalWeight_05.append(int(avg5/count1))
 		Totalweight_5.pop(0)
 		Totalweight_5.append(d2)
 		avg = avg+d2
 		dates.append(d1)
 		Totalweight.append(d2)
-		w_avg10.append(int(avg/10))
+		w_avg10.append(int(avg/count2))
 		isListFull =True
 		finalDate.append(d1)
-		finalWeight_10.append(int(avg/10))
+		finalWeight_10.append(int(avg/count2))
 	elif (isListFull == True):
 		w_avg5.pop(0)
 		avg5 = avg5+d2-Totalweight_5[0]
-		w_avg5.append(int(avg5/5))
-		finalWeight_05.append(int(avg5/5))
+		w_avg5.append(int(avg5/count1))
+		finalWeight_05.append(int(avg5/count1))
 		Totalweight_5.pop(0)
 		Totalweight_5.append(d2)
 		dates.pop(0)
@@ -76,28 +83,50 @@ for row in records:
 		w_avg10.pop(0)
 		dates.append(d1)
 		Totalweight.append(d2)
-		w_avg10.append(int(avg/10))
+		w_avg10.append(int(avg/count2))
 		finalDate.append(d1)
-		finalWeight_10.append(int(avg/10))
+		finalWeight_10.append(int(avg/count2))
 
 #print(finalWeight_05)
 #print(finalWeight_10)
 buyTrade = False
 sellTrade=False
-for (a,b,c) in zip(finalDate,finalWeight_05,finalWeight_10):
+buyPrice=0
+sellPrice=0
+result=''
+success_trade=0
+failed_trade=0
+tradeCount=0
+tabularData=[("Buy Date","Buy Price","SellPrice","Sell Date","Profit/Loss","Success/Fail")]
+buyDate = ''
+sellDate=''
+
+for (a,b,c,d) in zip(finalDate,finalWeight_05,finalWeight_10,price):
 	if(buyTrade==False and b>c):
-		print ("Buy stock -- "+ "5 Day SMA "+ str(b) + " crosses 10 Day SMA "+str(c) + "on " +str(a))
+		#print ("Buy stock -- "+str(count1)+ " Day SMA "+ str(b) + " crosses "+str(count2)+" Day SMA "+str(c) + "on " +str(a))
+		buyPrice = d
 		buyTrade = True
 		sellTrade=False
-	elif(sellTrade == False and b<c):
-		print("sell  Stock -- ""5 Day SMA "+ str(b) + " crosses 10 Day SMA "+str(c) + "on " +str(a))
+		buyDate =a
+	elif(buyTrade==True and sellTrade == False and b<c):
+		#print("sell  Stock -- "+str(count2)+" Day SMA "+ str(b) + " crosses "+str(count1)+" Day SMA "+str(c) + "on " +str(a))
+		sellDate =a
+		sellPrice = d
+		if(buyPrice<=sellPrice):
+			result='Success'
+			success_trade = success_trade+1
+		else:
+			result='Fail'
+			failed_trade = failed_trade+1
+		tradeCount = tradeCount+1
+		tabularData.append([str(buyDate),buyPrice,sellPrice,str(sellDate),round((sellPrice-buyPrice)*100/buyPrice,2),result])
 		sellTrade=True
 		buyTrade=False
-		
-
-
-
-
-
-# plt.plot(finalDate,finalWeight_10)
+if(buyTrade==True):
+	tabularData.append([str(buyDate),buyPrice,"","",""])
+print(tabulate(tabularData,headers="firstrow"))
+print("\n\nTotal Successful Trades :" + str(success_trade))
+print("Total Failed Trades :" + str(failed_trade))
+print("Success Ratio : "+str(success_trade*100/tradeCount)+" %")
+# plt.plot(finalWeight_10[-50:],finalWeight_05[-50:])
 # plt.show()
---
+++ 1.1.0/test3.py
@@ -0,0 +1,122 @@
+Crossover of  5  SMA and  10  SMA 
+
+
+symbol        Success    Failures  Success Ratio    OpenTrade    Rec. Date    Price
+----------  ---------  ----------  ---------------  -----------  -----------  -------
+ADANIPORTS         54          91  37.24 %          Buy          26/11/19     378.0
+ADANIPOWER         36          58  38.3 %           No
+ALBK               69         109  38.76 %          No
+APOLLOHOSP         80         152  34.48 %          No
+ASHOKLEY           55          78  41.35 %          No
+ASIANPAINT         85          88  49.13 %          Buy          03/12/19     1750.95
+AUBANK             12          18  40.0 %           No
+AUROPHARMA         79         115  40.72 %          Buy          22/11/19     449.7
+AXISBANK           89         122  42.18 %          No
+BAJAJ-AUTO         22          39  36.07 %          Buy          28/11/19     3216.75
+BAJAJHLDNG         96         141  40.51 %          No
+BAJFINANCE         72          82  46.75 %          No
+BANKINDIA          83         114  42.13 %          No
+BATAINDIA          78         117  40.0 %           Buy          04/12/19     1696.3
+BEL                68          72  48.57 %          No
+BERGEPAINT         63          71  47.01 %          Buy          27/11/19     503.35
+BHARTIARTL         83         128  39.34 %          Buy          18/11/19     420.55
+BPCL               88         132  40.0 %           No
+BRITANNIA          89         123  41.98 %          Buy          03/12/19     3116.0
+CANBK              82         118  41.0 %           No
+CASTROLIND         53          92  36.55 %          No
+CESC               86         121  41.55 %          Buy          03/12/19     749.55
+CHOLAFIN           62          83  42.76 %          No
+COROMANDEL         78         118  39.8 %           Buy          29/11/19     476.85
+CROMPTON            4           5  44.44 %          Buy          04/12/19     257.4
+CUMMINSIND         75         141  34.72 %          No
+DLF                54          90  37.5 %           Buy          16/10/19     160.85
+DRREDDY           108         158  40.6 %           No
+EICHERMOT          98         154  38.89 %          No
+EMAMILTD           54         101  34.84 %          Buy          03/12/19     336.0
+FCONSUMER          20          36  35.71 %          Buy          28/11/19     26.4
+FRETAIL            16          25  39.02 %          Buy          29/11/19     389.4
+GICRE               6          19  24.0 %           Buy          27/11/19     269.6
+GLENMARK           84         117  41.79 %          No
+GMRINFRA           33          62  34.74 %          Buy          22/10/19     19.65
+GODREJAGRO         11          18  37.93 %          Buy          27/11/19     489.0
+GODREJCP           74         116  38.95 %          No
+GODREJIND          75         101  42.61 %          No
+GODREJPROP         52          75  40.94 %          No
+GRAPHITE           64         113  36.16 %          No
+GRASIM             94         103  47.72 %          No
+HCLTECH            76         120  38.78 %          No
+HDFC               90         137  39.65 %          Buy          25/11/19     2297.0
+HDFCLIFE            9          21  30.0 %           No
+HEXAWARE           68          99  40.72 %          No
+HINDUNILVR        101         139  42.08 %          No
+HINDZINC           52          87  37.41 %          Buy          20/11/19     216.95
+IBULHSGFIN         34          46  42.5 %           Buy          25/11/19     266.0
+IBVENTURES         36          63  36.36 %          Buy          29/10/19     99.95
+ICICIGI            11          17  39.29 %          Buy          04/12/19     1380.0
+IDEA               44          71  38.26 %          Buy          20/11/19     8.15
+IGL                63          93  40.38 %          Buy          04/12/19     421.55
+INDIANB            53          96  35.57 %          Buy          22/11/19     121.35
+INDIGO             24          30  44.44 %          No
+INDUSINDBK         74         126  37.0 %           Buy          22/11/19     1460.8
+INFY              101         144  41.22 %          Buy          05/12/19     715.9
+ITC                74         110  40.22 %          No
+JINDALSTEL         75         118  38.86 %          No
+JSWENERGY          45          65  40.91 %          Buy          28/11/19     75.8
+JUBLFOOD           49          82  37.4 %           No
+KOTAKBANK          92         102  47.42 %          Buy          02/12/19     1636.5
+L&TFH              36          55  39.56 %          Buy          25/11/19     104.55
+LICHSGFIN          70         107  39.55 %          Buy          26/11/19     446.0
+LUPIN              89         118  43.0 %           Buy          21/11/19     787.95
+M&M                94         119  44.13 %          No
+MANAPPURAM         38          50  43.18 %          No
+MARICO             63         110  36.42 %          No
+MARUTI             87         116  42.86 %          No
+MFSL               76         140  35.19 %          Buy          06/11/19     443.45
+MGL                18          25  41.86 %          No
+MINDTREE           63          88  41.72 %          Buy          04/12/19     750.0
+MOTHERSUMI         42          75  35.9 %           No
+MPHASIS            90         128  41.28 %          No
+MRF                93         148  38.59 %          No
+MUTHOOTFIN         37          67  35.58 %          No
+NATIONALUM         84          96  46.67 %          Buy          04/12/19     43.8
+NAUKRI             83         106  43.92 %          No
+NESTLEIND          66          86  43.42 %          No
+NMDC               43          84  33.86 %          Buy          26/11/19     104.15
+NTPC               58         109  34.73 %          No
+OIL                42          78  35.0 %           No
+ONGC               74         144  33.94 %          No
+PAGEIND            81         100  44.75 %          Buy          05/12/19     22209.8
+PFIZER             90         128  41.28 %          Buy          02/12/19     4260.0
+PIDILITIND         71          98  42.01 %          Buy          02/12/19     1324.85
+PIIND              45          71  38.79 %          Buy          29/11/19     1500.0
+PNBHOUSING         10          27  27.03 %          No
+POWERGRID          41          98  29.5 %           No
+QUESS              16          24  40.0 %           No
+RAMCOCEM           73         115  38.83 %          No
+RBLBANK            17          26  39.53 %          Buy          01/11/19     324.8
+SAIL               72         111  39.34 %          Buy          26/11/19     40.1
+SBILIFE            13          21  38.24 %          Buy          29/11/19     970.0
+SBIN               79         114  40.93 %          No
+SIEMENS            83         141  37.05 %          No
+STRTECH            69         106  39.43 %          Buy          02/12/19     129.5
+SUNPHARMA          91         111  45.05 %          No
+SUNTV              64         122  34.41 %          No
+SYNGENE            19          38  33.33 %          No
+TATACHEM          104         125  45.41 %          Buy          20/11/19     654.3
+TATAMOTORS         87         126  40.85 %          No
+TATASTEEL          93         122  43.26 %          No
+TCS                71         115  38.17 %          No
+TECHM              67         108  38.29 %          No
+TITAN              73         105  41.01 %          Buy          28/11/19     1172.0
+TORNTPHARM         86         124  40.95 %          Buy          27/11/19     1899.8
+TORNTPOWER         55          96  36.42 %          No
+UBL                56         101  35.67 %          No
+UNIONBANK          67         134  33.33 %          Buy          21/11/19     59.1
+UPL                81         116  41.12 %          Buy          25/11/19     547.75
+VARROC              4          12  25.0 %           No
+VGUARD             39          52  42.86 %          No
+VOLTAS             68          87  43.87 %          No
+WIPRO              85         130  39.53 %          No
+YESBANK            58          85  40.56 %          Buy          29/11/19     74.0
+[Finished in 17.9s]
+[Finished in 18.0s]
