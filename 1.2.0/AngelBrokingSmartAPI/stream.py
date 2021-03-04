d1f7bd8242458126eef49a9e7577c9e18a779212
dipeshbodas <dipesh.bodas@gmail.com>
2021-03-04 15:21:44 +0530

    candle code continues

--- 1.2.0/AngelBrokingSmartAPI/stream.py
+++ 1.2.0/AngelBrokingSmartAPI/stream.py
@@ -5,6 +5,8 @@ from datetime import datetime
 import os
 from tabulate import tabulate 
 
+
+global candleData,mycursor,candle_start_times,candle_start_seconds,new_candle_flag,start_program_flag
 candleData={}
 obj=SmartConnect(api_key="tKace7Mp")
 data = obj.generateSession("D43726","Angel@1986")
@@ -12,7 +14,7 @@ FEED_TOKEN=obj.getfeedToken()
 CLIENT_CODE="D43726"
 cur_candle=[]
 mycursor = t.mydb.cursor()
-symQuery = "select symbol,token from algo_symbols limit 10"
+symQuery = "select symbol,token from algo_symbols limit 3"
 mycursor.execute(symQuery)
 data = mycursor.fetchall()
 token=""
@@ -24,41 +26,43 @@ new_candle_flag = {}
 start_program_flag=True
 
 
-def form_candle(time,sym,ltp):
-	if((str(time.minute) in candle_start_times and str(time.second)==candle_start_seconds and (new_candle_flag[sym]!="close")) or start_program_flag):
-		temp=candleData[sym]
-		sqlQuery = "insert into candle_data(symbol,closingtime,open,close,high,low)values(%s,%s,%s,%s,%s,%s)"
-		values=(sym,time,temp[0],temp[1],temp[2],temp[3])
-		mycursor.execute(sqlQuery,values)
-		print["Record inserted for :"+sym]
-		candleData={}
+def form_candle(time,sym,ltp,candleData):
+	tabular = [("symbol","time","open","close","high","low")]
+	keys = candleData.keys()
+	if((str(time.minute) in  candle_start_times and str(time.second)==candle_start_seconds and ( new_candle_flag[sym]!="close")) or len(candleData)==0 or sym not in keys):
+		if(len(candleData)!=0 and sym in keys):
+			temp= candleData[sym]
+			sqlQuery = "insert into candle_data(symbol,closingtime,open,close,high,low)values(%s,%s,%s,%s,%s,%s)"
+			values=(sym,time,temp[0],temp[1],temp[2],temp[3])
+			mycursor.execute(sqlQuery,values)
+			print("Record inserted for :",sym)
+			candleData={}
 		candleData[sym] = [ltp,ltp,ltp,ltp]
 		new_candle_flag[sym]="close"
-		start_program_flag=False
+		print("start for : ",sym)
 
 	else:
-		#print("Continue the current candle")
-	#tabular = [("symbol","time""open","close","high","low")]
-	#candleData = {"symbol" , {open,close,high,low}}
-		#keys = candleData.keys()
-		insertData = candleData[sym]
-		high=insertData[2]
-		low=insertData[3]
-		if(ltp < insertData[3]):
-			low=ltp
-		if(ltp > insertData[2]):
-			high=ltp
-		candleData[sym]=[insertData[0],insertData[1],high,low]
-		if(str(time.minute) not in candle_start_times):
-			new_candle_flag[sym]="open"
+		keys = candleData.keys()
+		if(sym in keys):
+			insertData =  candleData[sym]
+			high=insertData[2]
+			low=insertData[3]
+			if(ltp < insertData[3]):
+				low=ltp
+			if(ltp > insertData[2]):
+				high=ltp
+			candleData[sym]=[insertData[0],insertData[1],high,low]
+			if(str(time.minute) not in candle_start_times):
+				new_candle_flag[sym]="open"
+		print("continue for : ",sym)
 
-	#keys = candleData.keys()
+	# keys = candleData.keys()
 	# for key in keys:
 	# 	insertData = candleData[key]
 	# 	tempTab = (key,time.minute,insertData[0],insertData[1],insertData[2],insertData[3])
 	# 	tabular.append(tempTab)
-	#os.system('cls')
-	#print(tabulate(tabular,headers="firstrow"))
+	# os.system('cls')
+	# print(tabulate(tabular,headers="firstrow"))
 
 
 
@@ -85,7 +89,7 @@ def on_tick(ws, tick):
 				cur_symbol = list(symToken.keys())[list(symToken.values()).index(symboltick["tk"])]
 				tempData=(str(datetime.now().strftime("%H:%M:%S")),cur_symbol,str(symboltick["ltp"]))
 				tabulardata.append(tempData)
-				form_candle(datetime.now(),cur_symbol,float(symboltick["ltp"]))
+				form_candle(datetime.now(),cur_symbol,float(symboltick["ltp"]),candleData)
 		#os.system('cls')
 		#print(tabulate(tabulardata,headers="firstrow"))
 		
